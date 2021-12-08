c38dabe5782e175d5993c028ae0f682cb49b54d3
Jerome Swannack <jeromeswannack@gmail.com>
2021-12-09 07:40:25 +1300

    First cut

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # goliv
-A little service to help Liv with her notes project
+A little service to help Liv with her notes project.
+
+This is just for testing / playing around, so there is no persistent storage. This means every time you restart the 
+service the notes will be reset.
+
+## Caveats / Notes
+
+Some massive short cuts taken just to get you started
+
+* IDs are not managed by the service
+* The User field does nothing at this point (no auth, filtering, or anything)
+
+## Data Model
+
+```go
+type note struct {
+	ID   string   `json:"id"`
+	Text string   `json:"text"`
+	Tags []string `json:"tags"`
+	User string   `json:"user"`
+}
+```
+
+## Endpoints
+
+All endpoints return data as JSON
+
+* GET `/notes`
+  * Returns all notes
+  * The `tag` query parameter can be used to filter notes, i.e. `/notes?tag=goal&tag=family`
+* GET `/notes/:id`
+  * Gets an individual note by it's ID
+* POST `/notes`
+  * Add a note, body of request should be JSON note payload
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type note struct {
+	ID   string   `json:"id"`
+	Text string   `json:"text"`
+	Tags []string `json:"tags"`
+	User string   `json:"user"`
+}
+
+var notes = []note{
+	{
+		ID:   "1",
+		Text: "I want to make an effort to talk to my family more",
+		Tags: []string{"family", "goal"},
+		User: "Jerome",
+	},
+	{
+		ID:   "2",
+		Text: "Listen to Philosophize This! Philosophy is awesome",
+		Tags: []string{"philosophy", "goal", "podcasts"},
+		User: "Jerome",
+	},
+	{
+		ID:   "3",
+		Text: "Do washing this weekend",
+		Tags: []string{"important", "task", "soon"},
+		User: "Jerome",
+	},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/notes", getNotes)
+	router.GET("/notes/:id", getNoteByID)
+	router.POST("/notes", postNotes)
+
+	router.Run("localhost:8080")
+}
+
+// getNotes resposnds with the list of all Notes as JSON.
+func getNotes(c *gin.Context) {
+	requiredTags, any := c.GetQueryArray("tag")
+	if !any {
+		c.IndentedJSON(http.StatusOK, notes)
+		return
+	}
+
+	filteredNotes := []note{}
+
+	for _, n := range notes {
+		if hasAllTags(n.Tags, requiredTags) {
+			filteredNotes = append(filteredNotes, n)
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, filteredNotes)
+}
+
+func hasAllTags(tags, requiredTags []string) bool {
+	for _, t := range requiredTags {
+		if !hasTag(tags, t) {
+			return false
+		}
+	}
+	return true
+}
+
+func hasTag(tags []string, tag string) bool {
+	for _, t := range tags {
+		if tag == t {
+			return true
+		}
+	}
+	return false
+}
+
+// postNotes adds an note from JSON received in the request body
+func postNotes(c *gin.Context) {
+	var newNote note
+
+	// Call BindJSON to bind the received JSON to newnote
+	if err := c.BindJSON(&newNote); err != nil {
+		return
+	}
+
+	notes = append(notes, newNote)
+	c.IndentedJSON(http.StatusCreated, newNote)
+}
+
+// getNoteByID locates the note whose ID value matches the id
+// parameter sent by the client, then returns that note as a response.
+func getNoteByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range notes {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "note not found"})
+}
